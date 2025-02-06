ad1f62902f14643b69ac463852f3a353b2799b63
wjbeckett <wjbeckett@gmail.com>
2025-02-06 14:26:12 +1000

    fixed GameCard component location

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import GameCard from '../components/GameCard';
+import GameCard from './components/GameCard';
 
 export default function Library() {
   const [games, setGames] = useState([]);

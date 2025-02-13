e23584e07c18485a518fd50cbe84bc1f200e42e1
wjbeckett <wjbeckett@gmail.com>
2025-02-13 16:35:39 +1000

    trying to fix the JSON validation issue.

---
+++ src/utils/validateJson.js
@@ -0,0 +1,12 @@
+const validateGameJson = (req, res, next) => {
+    try {
+      if (req.body.metadata && typeof req.body.metadata === 'string') {
+        req.body.metadata = JSON.parse(req.body.metadata);
+      }
+      next();
+    } catch (error) {
+      res.status(400).json({ error: 'Invalid metadata format' });
+    }
+  };
+  
+module.exports = { validateGameJson };

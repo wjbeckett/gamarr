3d3a6de66945b9b493daad88ecd80a1e921d2320
wjbeckett <wjbeckett@gmail.com>
2025-02-13 16:17:32 +1000

    trying to fix validateJSON issue..

--- src/utils/validateJson.js
+++ src/utils/validateJson.js
@@ -1,16 +1,12 @@
-export const validateGameJson = (req, res, next) => {
-    const validate = (data) => {
-      return typeof data.metadata === 'string' ? 
-        JSON.parse(data.metadata) : 
-        data.metadata;
-    };
-  
-    try {
-      if (req.body.metadata) {
-        req.body.metadata = validate(req.body.metadata);
-      }
-      next();
-    } catch (error) {
-      res.status(400).json({ error: 'Invalid metadata format' });
+const validateGameJson = (req, res, next) => {
+  try {
+    if (req.body.metadata && typeof req.body.metadata === 'string') {
+      req.body.metadata = JSON.parse(req.body.metadata);
     }
-  };
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid metadata format' });
+  }
+};
+
+module.exports = { validateGameJson };
